f0ef872707d336dd4802330ce1706825cce068b8
YugalGautam1 <reachyugal@gmail.com>
2025-06-07 20:58:08 -0400

    Updating

--- housing.py
+++ housing.py
@@ -1,14 +1,31 @@
 import pandas as pd
 import numpy as np 
 from sklearn import linear_model
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+
+
 df = pd.read_csv('data/housing_prices/train.csv')
+df['YearDifference'] = df['YrSold'] - df['YearBuilt']
+
 print(df.head())
+plt.figure(figsize=(10, 6))
+plt.scatter(df['YearDifference'], df['SalePrice'], alpha=0.5, color='b')
+plt.show()
+poly = PolynomialFeatures(degree=2)
+X_poly = poly.fit_transform(df[['LotArea', 'YearDifference', 'OverallCond', 'YrSold']])
 
 reg = linear_model.LinearRegression()
-reg.fit(df[['LotArea', 'YearBuilt', 'OverallCond','YrSold']], df.SalePrice)
-df2 = pd.read_csv('data/housing_prices/test.csv')
+reg.fit(X_poly, df['SalePrice'])
 
-predictions = reg.predict(df2[['LotArea', 'YearBuilt', 'OverallCond','YrSold']])
+
+
+df2 = pd.read_csv('data/housing_prices/test.csv')
+df2['YearDifference'] = df2['YrSold'] - df2['YearBuilt']
+
+X_poly_test = poly.transform(df2[['LotArea', 'YearDifference', 'OverallCond', 'YrSold']])
+
+predictions = reg.predict(X_poly_test)
 print(predictions)
 a = pd.DataFrame({
     'Id': df2['Id'],
