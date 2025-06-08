99a73400d53e2ccc97b908c48eb86475210efa2b
YugalGautam1 <reachyugal@gmail.com>
2025-06-08 03:24:38 -0400

    Trying again

--- housing.py
+++ housing.py
@@ -1,35 +1,46 @@
 import pandas as pd
-from sklearn import linear_model
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import PolynomialFeatures
-
+from sklearn.ensemble import GradientBoostingRegressor
 
 df = pd.read_csv('data/housing_prices/train.csv')
 df['YearDifference'] = df['YrSold'] - df['YearBuilt']
-print(df['SaleType'].unique())
-df['Street'] = df['Street'].replace('Grvl', 0)
-df['Street'] = df['Street'].replace('Pave', 1)
+df['Street'] = df['Street'].replace({'Grvl': 0, 'Pave': 1})
 
-poly = PolynomialFeatures(degree=2)
-X_poly = poly.fit_transform(df[['LotArea', 'YearDifference', 'OverallCond', 'YrSold','OverallQual','MSSubClass','Street']])
-
-
-reg = linear_model.LinearRegression()
-reg.fit(X_poly, df['SalePrice'])
-
-
+features = ['LotArea', 'OverallCond', 'YrSold', 'OverallQual', 'MSSubClass', 'Street','SaleType']
 
 df2 = pd.read_csv('data/housing_prices/test.csv')
 df2['YearDifference'] = df2['YrSold'] - df2['YearBuilt']
-df2['Street'] = df2['Street'].replace('Grvl', 0)
-df2['Street'] = df2['Street'].replace('Pave', 1)
-X_poly_test = poly.transform(df2[['LotArea', 'YearDifference', 'OverallCond', 'YrSold','OverallQual','MSSubClass','Street']])
+df2['Street'] = df2['Street'].replace({'Grvl': 0, 'Pave': 1})
+sale_type_map = {
+    'WD': 0,     
+    'CWD': 1,    
+    'VWD': 2,    
+    'New': 3,    
+    'COD': 4,   
+    'Con': 5,    
+    'ConLw': 6,  
+    'ConLI': 7,  
+    'ConLD': 8, 
+    'Oth': 9,
+}
+df['SaleType'] = df['SaleType'].map(sale_type_map).fillna(9)
+df2['SaleType'] = df2['SaleType'].map(sale_type_map).fillna(9)
 
-predictions = reg.predict(X_poly_test)
-print(predictions)
-a = pd.DataFrame({
+
+X_train = df[features]
+y_train = df['SalePrice']
+
+X_test = df2[features]
+
+gb_model = GradientBoostingRegressor(
+    n_estimators=100,
+    learning_rate=0.1,
+    max_depth=3,
+)
+gb_model.fit(X_train, y_train)
+
+gb_preds = gb_model.predict(X_test)
+
+pd.DataFrame({
     'Id': df2['Id'],
-    'SalePrice': predictions
-})
-
-a.to_csv('data/housing_prices/fin.csv', index=False)
+    'SalePrice': gb_preds
+}).to_csv('data/housing_prices/fin.csv', index=False)
