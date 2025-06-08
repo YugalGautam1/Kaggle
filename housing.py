a9782bf6f35aad424547a1d5f324075813739e44
YugalGautam1 <reachyugal@gmail.com>
2025-06-08 04:20:32 -0400

    Becoming better

--- housing.py
+++ housing.py
@@ -9,16 +9,15 @@ df2 = pd.read_csv('data/housing_prices/test.csv')
 df['YearDifference'] = df['YrSold'] - df['YearBuilt']
 df2['YearDifference'] = df2['YrSold'] - df2['YearBuilt']
 
-df['Street'] = df['Street'].replace({'Grvl': 0, 'Pave': 1})
-df2['Street'] = df2['Street'].replace({'Grvl': 0, 'Pave': 1})
 
-features = ['LotArea', 'OverallCond', 'YrSold', 'OverallQual', 'MSSubClass', 'Street', 'SaleType', 'MSZoning', 'YearDifference']
+df2['YearDifference'] = df2['YrSold'] - df2['YearBuilt']
+features = ['Neighborhood','BldgType','Foundation','SaleType','SaleCondition','MSSubClass','LotArea','OverallCond','OverallQual','YearBuilt','YearDifference']
 
 df = df[['Id']+features + ['SalePrice']]
 df2 = df2[['Id']+features]
 
-df = pd.get_dummies(df, columns=['SaleType', 'MSZoning'])
-df2 = pd.get_dummies(df2, columns=['SaleType', 'MSZoning'])
+df = pd.get_dummies(df, columns=['Neighborhood','BldgType','Foundation','SaleType','SaleCondition'])
+df2 = pd.get_dummies(df2, columns=['Neighborhood','BldgType','Foundation','SaleType','SaleCondition'])
 
 X_train = df.drop('SalePrice', axis=1)
 y_train = df['SalePrice']
